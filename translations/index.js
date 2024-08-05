af718cfb3ac80e84a838f0f4e9e2aa7241a99acb
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-08-05 06:16:25 +0200

    Add files via upload

---
+++ translations/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	JA: require('./ja.json'),
+	EN: require('./en.json'),
+	FR: require('./fr.json'),
+	DE: require('./de.json'),
+	IT: require('./it.json'),
+	ES: require('./es.json'),
+	RU: require('./ru.json'),
+
+};
